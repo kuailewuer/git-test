894cd11c70690d8c2f8adacf830f391f599dfee3
test <test@atguigu.com>
2023-10-23 16:07:41 +0800

    first commit

---
+++ src/main/java/com.atguigu.git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wuliyun
+ * @date 2023年10月23日 16:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git commit 1");
+    }
+}
