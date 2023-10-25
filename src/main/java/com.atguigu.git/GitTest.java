d81489f14500c2f642313db7e7d1bd3cc17a9978
test <test@atguigu.com>
2023-10-25 10:29:53 +0800

    hot-fix 2 commit

--- src/main/java/com.atguigu.git/GitTest.java
+++ src/main/java/com.atguigu.git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("git commit 2");
         System.out.println("git commit 3");
         System.out.println("git commit 4");
+        System.out.println("git commit 5");
     }
 }
